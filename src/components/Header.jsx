70d9c767be7f8c08f948a4ee4cd99379fd8dcc82
WaseemWisa <wasee3wesa@gmail.com>
2023-09-30 00:27:48 +0200

    nav update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import '../Styles/components/header.scss'
 
 function Header() {
   const [isActive, setIsActive] = useState(false);
+  const toggleIsActive = () => setIsActive(false);
   return (
     <header id='header'>
       <div className="container">
@@ -11,10 +12,10 @@ function Header() {
           <i className="bi bi-list mob-i" onClick={() => setIsActive(!isActive)}></i>
         </div>
         <nav className={isActive ? `mob-links` : 'none'}>
-          <a href="#">Home</a>
-          <a href="#about">About</a>
-          <a href="#projects">Projects</a>
-          <a href="#contact">Contact</a>
+          <a href="#" onClick={toggleIsActive}>Home</a>
+          <a href="#about" onClick={toggleIsActive}>About</a>
+          <a href="#projects" onClick={toggleIsActive}>Projects</a>
+          <a href="#contact" onClick={toggleIsActive}>Contact</a>
         </nav>
       </div>
     </header>
